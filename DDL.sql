35705f069c1e409d66708bb393590d9db71fee12
Kassem Bagher <kassem@waqood.sa>
2017-04-17 16:55:07 +1000

    - fields type enhancement. - added missing constraints

--- DDL.sql
+++ DDL.sql
@@ -20,13 +20,13 @@ CREATE TABLE supermarket.customer_order
   order_subtotal DECIMAL DEFAULT '0' NULL,
   order_total    DECIMAL DEFAULT '0' NULL,
   order_discount DECIMAL DEFAULT '0' NULL,
-  order_status   VARCHAR(20)         NULL
+  order_status   VARCHAR(20)         NULL,
+  CONSTRAINT customer_order_customer_cust_id_fk
+  FOREIGN KEY (customer) REFERENCES supermarket.customer (cust_id)
 );
 
-COMMENT ON COLUMN customer_order.status IS 'pending
-placed
-canceled'
-;
+CREATE INDEX customer_order_customer_cust_id_fk
+  ON customer_order (customer);
 
 CREATE TABLE supermarket.discount
 (
@@ -46,10 +46,15 @@ CREATE TABLE supermarket.employee
   emp_name     VARCHAR(200) NULL,
   emp_password VARCHAR(256) NULL,
   emp_username VARCHAR(50)  NULL,
-  emp_role     INT          NULL,
+  emp_role     VARCHAR(20)  NULL,
   CONSTRAINT employee_emp_username_uindex
   UNIQUE (emp_username)
 );
+
+COMMENT ON COLUMN employee.emp_role IS 'manager
+sales
+warehouse'
+;
 
 CREATE TABLE supermarket.order_item
 (
@@ -63,6 +68,9 @@ CREATE TABLE supermarket.order_item
 
 CREATE INDEX order_item_order_id_fk
   ON order_item (cust_order);
+
+CREATE INDEX order_item_product_prod_id_fk
+  ON order_item (product);
 
 CREATE TABLE supermarket.product
 (
@@ -79,12 +87,12 @@ CREATE TABLE supermarket.product
 CREATE INDEX product_supplier_id_fk
   ON product (supplier);
 
-COMMENT ON COLUMN product.type IS 'pcs
-kg'
-;
-
 ALTER TABLE discount
   ADD CONSTRAINT discount_product_id_fk
+FOREIGN KEY (product) REFERENCES supermarket.product (prod_id);
+
+ALTER TABLE order_item
+  ADD CONSTRAINT order_item_product_prod_id_fk
 FOREIGN KEY (product) REFERENCES supermarket.product (prod_id);
 
 CREATE TABLE supermarket.purchase_order
@@ -109,8 +117,13 @@ CREATE TABLE supermarket.purchase_order_item
   purchase_order INT                 NULL,
   product        INT                 NULL,
   item_quantity  INT                 NULL,
-  item_total     DECIMAL DEFAULT '0' NULL
+  item_total     DECIMAL DEFAULT '0' NULL,
+  CONSTRAINT purchase_order_item_purchase_order_prd__id_fk
+  FOREIGN KEY (purchase_order) REFERENCES supermarket.purchase_order (prd__id)
 );
+
+CREATE INDEX purchase_order_item_purchase_order_prd__id_fk
+  ON purchase_order_item (purchase_order);
 
 CREATE TABLE supermarket.supplier
 (
@@ -129,3 +142,4 @@ FOREIGN KEY (supplier) REFERENCES supermarket.supplier (sup_id);
 ALTER TABLE purchase_order
   ADD CONSTRAINT purchase_order_supplier_id_fk
 FOREIGN KEY (supplier) REFERENCES supermarket.supplier (sup_id);
+
